6c843369a297b5294347776fc95a9dc3e965ec29
Vasilliy <Vasily.vvk@gmail.com>
2022-10-26 10:47:56 +0300

    Done

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,19 +2,29 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        int first = 1;
-        int second = 10;
-        int third = 100;
-        System.out.println(first + "\n" + second + "\n" + third);
-        int linkToFirst = first;
-        int linkToSecond = second;
-        int linkToThird = third;
+        int first=1;
+        int second=10;
+        int third=100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
         first = 15;
         second = 6;
         third = 4;
-        System.out.println(first + "\n" + second + "\n" + third + "\n" +
-                linkToFirst + "\n" + linkToSecond + "\n" + linkToThird + "\n" +
-                first + "\n" + second + "\n" + third);
+        int linkToFirst = first = 15;
+        int linkToSecond = second = 6;
+        int linkToThird  = third = 4;
+
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
 
 
         //System.out.println("1\n10\n100\n15\n6\n4\n1\n10\n100\n");
